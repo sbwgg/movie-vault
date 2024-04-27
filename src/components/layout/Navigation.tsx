0cce0ea91bdbc76f42ede357672752532f46e78e
levrx <jvcke@icloud.com>
2024-04-27 15:29:40 +0100

    and more changes

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -87,6 +87,14 @@ export function Navigation(props: NavigationProps) {
                 <BrandPill clickable />
               </Link>
               <a
+                href={conf().GITHUB_LINK}
+                target="_blank"
+                rel="noreferrer"
+                className="text-xl text-white tabbable rounded-full"
+              >
+                <IconPatch icon={Icons.GITHUB} clickable downsized />
+              </a>
+              <a
                 href={conf().DISCORD_LINK}
                 target="_blank"
                 rel="noreferrer"
