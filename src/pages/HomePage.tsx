83289ab6f4638aa33e2d617e2ebc4b63a5f5a7cb
levrx <jvcke@icloud.com>
2024-04-28 21:31:41 +0100

    added disclaimer for NSBX
    just while extension doesn't like NSBX

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 
 import { WideContainer } from "@/components/layout/WideContainer";
-import { useDebounce } from "@/hooks/useDebounce";
 import { useSearchQuery } from "@/hooks/useSearchQuery";
 import { HomeLayout } from "@/pages/layouts/HomeLayout";
 import { BookmarksPart } from "@/pages/parts/home/BookmarksPart";
@@ -13,24 +12,32 @@ import { WatchingPart } from "@/pages/parts/home/WatchingPart";
 import { SearchListPart } from "@/pages/parts/search/SearchListPart";
 import { SearchLoadingPart } from "@/pages/parts/search/SearchLoadingPart";
 
-import { Icon, Icons } from "../components/Icon";
+interface NotificationProps {
+  message: string;
+  onClose: () => void;
+}
 
-function useSearch(search: string) {
-  const [searching, setSearching] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const debouncedSearch = useDebounce<string>(search, 500);
-  useEffect(() => {
-    setSearching(search !== "");
-    setLoading(search !== "");
-  }, [search]);
-  useEffect(() => {
-    setLoading(false);
-  }, [debouncedSearch]);
-  return {
-    loading,
-    searching,
+export function Notification({ message, onClose }: NotificationProps) {
+  const handleClose = () => {
+    onClose();
   };
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-#1f1f32 rounded-lg p-8 max-w-md relative">
+      <button
+  type="button"
+  className="absolute top-0 right-0 mt-2 mr-2 bg-rgba(30,30,51,255)-500 hover:bg-rgba(30,30,51,255)-600 text-white font-bold py-2 px-4 rounded"
+  onClick={handleClose}
+>
+  x
+</button>
+        <p className="font-bold text-xl text-center bg-gray-700 text-white p-2 rounded-md">
+          {message}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export function HomePage() {
@@ -38,25 +45,39 @@ export function HomePage() {
   const [showBg, setShowBg] = useState(false);
   const searchParams = useSearchQuery();
   const [search] = searchParams;
-  const { loading } = useSearch(search);
-  const navigate = useNavigate();
+  const [showNotification, setShowNotification] = useState(true);
 
-  const handleMouseEnter = (e: { currentTarget: { style: { transform: string; filter: string; }; }; }) => {
-    e.currentTarget.style.transform = 'scale(1.05)';
-    e.currentTarget.style.filter = 'brightness(1.2)';
+  const handleCloseNotification = () => {
+    setShowNotification(false);
   };
 
-  const handleMouseLeave = (e: { currentTarget: { style: { transform: string; filter: string; }; }; }) => {
-    e.currentTarget.style.transform = 'scale(1)';
-    e.currentTarget.style.filter = 'brightness(1)';
-  };
+  useEffect(() => {
+    // Disable scrolling when notification is active
+    if (showNotification) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Re-enable scrolling when component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showNotification]);
 
   return (
     <HomeLayout showBg={showBg}>
+      <Helmet>
+        <title>{t("global.name")}</title>
+      </Helmet>
+      {/* Notification */}
+      {showNotification && (
+        <Notification
+          message="If you want to use NSBX (our best source), please disable the extension as it interferes with this source."
+          onClose={handleCloseNotification}
+        />
+      )}
       <div className="mb-16 sm:mb-24">
-        <Helmet>
-          <title>{t("global.name")}</title>
-        </Helmet>
         <HeroPart searchParams={searchParams} setIsSticky={setShowBg} />
         <div style={{ textAlign: 'center' }}>
           <RouterLink
@@ -74,21 +95,20 @@ export function HomePage() {
               textDecoration: 'none',
               padding: '8px 12px', 
             }}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
           >
-            <div className="mr-1"> 
-              <Icon icon={Icons.MOVIE} />
-            </div>
-            <p style={{ margin: 0 }}>Discover Your Next Movie or Series</p>
+            <p className="font-bold text-buttons-secondaryText" style={{ fontSize: "18px" }}>🎥 Discover Your Next Movie or Series 🎥</p>
           </RouterLink>
         </div>
       </div>
       <WideContainer>
-        {loading ? <SearchLoadingPart /> : search ? <SearchListPart searchQuery={search} /> : <>
-          <BookmarksPart />
-          <WatchingPart />
-        </>}
+        {search ? (
+          <SearchListPart searchQuery={search} />
+        ) : (
+          <>
+            <BookmarksPart />
+            <WatchingPart />
+          </>
+        )}
       </WideContainer>
     </HomeLayout>
   );
